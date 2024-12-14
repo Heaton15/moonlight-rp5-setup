13bcdc058d4940b12e20e80762a589a36a75c46d
Tim H <Heaton15@gmail.com>
2024-12-14 17:46:50 -0500

    typo

--- xone.sh
+++ xone.sh
@@ -3,7 +3,8 @@ set -ex
 XONE="https://github.com/medusalix/xone"
 # Install xone for the xbox one remote
 git clone $XONE
+sudo apt install dkms
 pushd xone && sudo ./install.sh
-sudo xone-getfirmware.sh --skip-disclaimer
+sudo xone-get-firmware.sh --skip-disclaimer
 popd
 
