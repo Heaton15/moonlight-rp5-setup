39ab2bf0d741a393ced5c417a56c8bfa1604646d
Tim H <Heaton15@gmail.com>
2025-04-13 14:59:35 -0400

    The xone project we were pointing to is no longer maintained. Updating to an active fork with fixes that make the driver compatible with newer linux kernels.

--- xone.sh
+++ xone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-XONE="https://github.com/medusalix/xone"
+XONE="https://github.com/dlundqvist/xone"
 # Install xone for the xbox one remote
 git clone $XONE
 sudo apt install dkms cabextract
